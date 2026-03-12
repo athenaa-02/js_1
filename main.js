04a05214b226df8dd4ee83f2e763234b5996ee18
natia <karamnishvili.natia@gmail.com>
2026-03-12 18:51:41 +0400

    javascript variables

---
+++ main.js
@@ -0,0 +1,39 @@
+// console.log(167365980460 * 35487);
+
+// string - ტექსტი console.log("სჯჰედფკხზ");
+// number - რიცხვი  console.log(124);
+
+let age = 23;
+let plan = 7;
+
+// console.log(age + plan);
+
+// console.log("natia");
+
+// console.log("age");
+
+let currentYear = 2026;
+let birthYear = 2002;
+
+let myAge = currentYear - birthYear;
+
+// console.log(myAge);
+
+let name = "lorem ipsum";
+
+// console.log("name", age)
+
+let numberOne = 100;
+let numberTwo = 25;
+
+// console.log(numberOne % numberTwo)
+// console.log(numberOne % numberTwo == 0);
+
+// console.log('natia' == 'natiaa')
+console.log(numberOne == numberTwo);
+
+console.log("natia" == numberOne);
+
+console.log(20 / 5 + 4 - 3872654 * 263754);
+
+console.log(numberOne + numberTwo * numberOne);
